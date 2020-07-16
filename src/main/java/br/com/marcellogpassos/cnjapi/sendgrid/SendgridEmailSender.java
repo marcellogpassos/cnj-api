99a94e00dccfd84ccba355d22df438832b445e68
Marcello Passos <marcello.passos@tjpb.jus.br>
2020-07-15 22:29:13 -0300

    Refatoração na classe SendgridEmailSender

--- src/main/java/br/com/marcellogpassos/cnjapi/sendgrid/SendgridEmailSender.java
+++ src/main/java/br/com/marcellogpassos/cnjapi/sendgrid/SendgridEmailSender.java
@@ -7,10 +7,13 @@ import com.sendgrid.Email;
 import com.sendgrid.Mail;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Log4j2
 @Service
@@ -33,25 +36,35 @@ public class SendgridEmailSender {
 
     public void notificar(Interessado informed, Version novaVersao) {
         try {
-            final var content = String.format(this.text, informed.getNome(), novaVersao.getDataVersao().toString());
-            Mail mail = new Mail(
-                    new Email(this.from),
-                    this.subject,
-                    new Email(informed.getEmail()),
-                    new Content("text/plain", content));
-
-            final var sendgridRequest = new Request();
-            sendgridRequest.setMethod(Method.POST);
-            sendgridRequest.setEndpoint("mail/send");
-            sendgridRequest.setBody(mail.build());
-
+            final var mail = this.getMail(informed, novaVersao);
+            final var sendgridRequest = this.getRequest(mail);
             final var response = this.sg.api(sendgridRequest);
-
-            log.info(">>> response.statusCode: " + response.getStatusCode());
-            log.info(">>> response.body: " + response.getBody());
-            log.info(">>> response.headers: " + response.getHeaders());
+            this.handleResponse(response);
         } catch (Exception ex) {
             log.error(">>> Falha ao tentar enviar o e-mail. Causa: " + ex.getMessage(), ex);
         }
+    }
+
+    private Mail getMail(Interessado informed, Version novaVersao) {
+        final var content = String.format(this.text, informed.getNome(), novaVersao.getDataVersao().toString());
+        return new Mail(
+                new Email(this.from),
+                this.subject,
+                new Email(informed.getEmail()),
+                new Content("text/plain", content));
+    }
+
+    private Request getRequest(Mail mail) throws IOException {
+        final var sendgridRequest = new Request();
+        sendgridRequest.setMethod(Method.POST);
+        sendgridRequest.setEndpoint("mail/send");
+        sendgridRequest.setBody(mail.build());
+        return sendgridRequest;
+    }
+
+    private void handleResponse(Response response) {
+        log.info(">>> response.statusCode: " + response.getStatusCode());
+        log.info(">>> response.body: " + response.getBody());
+        log.info(">>> response.headers: " + response.getHeaders());
     }
 }
